895a019fd13c140c552d1540c1ca364d7244c8d5
Yusuf Alperen BOZKURT <108146039+YAlperenBOZKURT@users.noreply.github.com>
2022-07-29 00:13:27 +0300

    Add files via upload

---
+++ Homework 2 - Navbar/project.js
@@ -0,0 +1,14 @@
+
+
+        function toggle () {
+            let x = document.getElementById('navDiv');
+    
+            if(x.style.display == "none") 
+            {
+            x.style.display ="block";
+            }
+            else {
+            x.style.display ="none";
+             }
+    }
+    
